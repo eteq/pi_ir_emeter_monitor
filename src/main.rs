203c12ae9818f9147db729b1c5e98e103b3bc31a
Erik Tollerud <erik.tollerud@gmail.com>
2022-06-09 23:46:02 -0400

    sqlite3-based output

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use std::io::prelude::*;
-use std::fs::OpenOptions;
 use std::error::Error;
 //use std::thread::sleep;
 use std::time::{SystemTime, Duration, UNIX_EPOCH};
+
+use rusqlite::{Connection, Result};
 
 use rppal::gpio;
 
 const PULSE_PIN: u8 = 24; //BCM pin #
 
 
+fn setup_db(path: &str) -> Result<Connection>{
+    let conn = Connection::open(path)?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS wh_pulses (
+            id              INTEGER PRIMARY KEY,
+            tstampunixns            INTEGER NOT NULL
+        )",
+        [], // empty list of parameters.
+    )?;
+
+    Ok(conn)
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let log_path = "power_mon.log";
-    let mut outfile = OpenOptions::new().append(true).create(true).open(log_path)?;
+    let log_path = "power_mon.sqlite3";
+
+    let db = setup_db(log_path)?;
+    println!("Completed setup of sqlite db file {}", log_path);
 
     let g = gpio::Gpio::new()?;
 
@@ -45,9 +62,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
 
             let unix_time_nanos = t2.duration_since(UNIX_EPOCH)?.as_nanos() as u64;
-            //write!(outfile, "{}", unix_time_nanos)?;
-            outfile.write_all(&unix_time_nanos.to_le_bytes())?;
-            outfile.flush()?;
+            db.execute("INSERT INTO wh_pulses (tstampunixns) VALUES (?1)", 
+                       [&unix_time_nanos])?;
 
             t_last_pulse = t2;
         }
