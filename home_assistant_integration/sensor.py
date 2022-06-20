064674b950a262299a76fd18d600590cbd797948
Erik Tollerud <erik.tollerud@gmail.com>
2022-06-20 00:20:02 -0400

    add kwh sensor to home assistant integration

--- home_assistant_integration/sensor.py
+++ home_assistant_integration/sensor.py
@@ -14,7 +14,7 @@ from homeassistant.helpers.typing import (
     HomeAssistantType,
 )
 
-from homeassistant.const import CONF_HOST, POWER_KILO_WATT
+from homeassistant.const import CONF_HOST, POWER_KILO_WATT, ENERGY_KILO_WATT_HOUR
 
 from .const import DOMAIN
 
@@ -27,7 +27,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 _LOGGER = logging.getLogger(__name__)
 
-SCAN_INTERVAL = timedelta(seconds=30)
+SCAN_INTERVAL = timedelta(seconds=45)
 
 
 
@@ -43,21 +43,17 @@ async def async_setup_platform(
 
     hostname = config[CONF_HOST]
 
-    sensors = [PiIREmeterSensor(session, hostname)]
+    sensors = [PiIREmeterSensorKw(session, hostname), PiIREmeterSensorKwh(session, hostname, 60*24)]
 
     async_add_entities(sensors, update_before_add=True)
 
 
-class PiIREmeterSensor(SensorEntity):
+class PiIREmeterSensorBase(SensorEntity):
     def __init__(self, session, hostname):
         super().__init__()
         self.session = session
         self.hostname = hostname
-        self.endpoint = 'kw'
-
-    _attr_native_unit_of_measurement = POWER_KILO_WATT
-    _attr_device_class = SensorDeviceClass.POWER
-    _attr_state_class = SensorStateClass.MEASUREMENT
+        self.endpoint = None
 
     @property
     def name(self):
@@ -67,14 +63,43 @@ class PiIREmeterSensor(SensorEntity):
     def url(self):
         return 'http://' + self.hostname + '/' + self.endpoint
 
+    @property
+    def params_get(self):
+        return None
+
     async def async_update(self):
         try:
-            async with self.session.get(self.url) as resp:
+            async with self.session.get(self.url, params=self.params_get) as resp:
                 if resp.status == 200:
                     j = await resp.json()
                     self._attr_native_value = j['value']
                 else:
-                    _LOGGER.error(f"Error retrieving data from Pi IR Emeter - got URL status code {resp.status}.")
+                    _LOGGER.error(f"Error retrieving data from Pi IR Emeter at endpoint f{self.endpoint} - got URL status code {resp.status}.")
 
         except:
             _LOGGER.exception("Error retrieving data from Pi IR Emeter.")
+
+
+class PiIREmeterSensorKw(PiIREmeterSensorBase):
+    _attr_native_unit_of_measurement = POWER_KILO_WATT
+    _attr_device_class = SensorDeviceClass.POWER
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def __init__(self, session, hostname):
+        super().__init__(session, hostname)
+        self.endpoint = 'kw'
+
+
+class PiIREmeterSensorKwh(PiIREmeterSensorBase):
+    _attr_native_unit_of_measurement = ENERGY_KILO_WATT_HOUR
+    _attr_device_class = SensorDeviceClass.ENERGY
+    _attr_state_class = SensorStateClass.TOTAL
+
+    def __init__(self, session, hostname, minutes_back):
+        super().__init__(session, hostname)
+        self.endpoint = 'kwh_since'
+        self.minutes_back = minutes_back
+
+    @property
+    def params_get(self):
+        return {'minutes_back': self.minutes_back}
